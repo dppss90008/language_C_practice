ff9cc9f79b7c35436716c3bf8f4eaacf8906f17f
dppss90008 <34399929+dppss90008@users.noreply.github.com>
2020-01-14 00:08:08 +0800

    第二課更新

---
+++ C語言自學/6_printf.c
@@ -0,0 +1,21 @@
+# include<stdio.h>
+# include<stdlib.h>
+
+int main(void){
+
+	int a = 123456789;
+		unsigned int b = 3000000000;
+		
+		printf("%d\n",a);
+		// ทธฆ์ 
+		printf("%d\n",b);
+		printf("%u\n",b);
+
+	float c = 123.45;
+    double d = 123.45;
+    printf("%f\n",c); 
+    printf("%f\n",d);
+
+	return 0;
+}
+
